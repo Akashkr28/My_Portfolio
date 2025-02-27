a0bd622626b498b35b762caefca163c52b0a4c0e
Akashkr28 <email.akash2010@gmail.com>
2025-02-27 20:11:29 +0530

    small update

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Connect With Me</h4>
+        <h2 className='text-center text-5xl font-Ovo'>Get in Touch</h2>
+
+        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+            I'd love to connect with you! Whether you have a project in mind or just want to chat, feel free to get in touch. Let's collaborate and bring your ideas to life.
+        </p>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Services = () => {
   return (
-    <div id='services' className='w-full px-[12%] py-10 scroll mt-10'>
+    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>Work I Offer</h4>
         <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
 
