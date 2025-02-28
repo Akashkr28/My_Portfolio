ca2571bdd4601cbf5e69d93a3159e093e79b15c8
Akashkr28 <email.akash2010@gmail.com>
2025-02-28 21:35:48 +0530

    Added dark mode theme

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets, workData } from '../../assets/assets'
 import Image from 'next/image'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
@@ -12,7 +12,7 @@ const Work = () => {
             Welcome to my web development portfolio! Here, you'll find a collection of my latest projects, showcasing my skills and expertise in creating engaging and user-friendly web experiences.
         </p>
 
-        <div className='grid grid-cols-auto my-10 gap-5'>
+        <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
             {workData.map((project, index) => (
                 <div key={index} 
                     className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
@@ -30,8 +30,8 @@ const Work = () => {
             ))}
         </div>
         <a 
-            className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto hover:bg-lightHover duration-500'
-            href="">Show More <Image src={assets.right_arrow_bold} alt='right-arrow' className='w-4'/></a>
+            className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover/50'
+            href="">Show More <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='right-arrow' className='w-4'/></a>
     </div>
   )
 }
