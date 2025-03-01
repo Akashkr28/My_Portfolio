cb952210c115afada24ea9cbecb4d0d4aa52e2ac
Akashkr28 <email.akash2010@gmail.com>
2025-03-01 05:44:44 +0530

    Added motion framer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import Image from 'next/image'
+import { motion } from "motion/react"
 
 const Contact = () => {
 
@@ -30,27 +31,77 @@ const Contact = () => {
     };
 
   return (
-    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>Connect With Me</h4>
-        <h2 className='text-center text-5xl font-Ovo'>Get in Touch</h2>
+    <motion.div id='contact' 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1}}
+        className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'
 
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+    >
+        <motion.h4 
+            className='text-center mb-2 text-lg font-Ovo'
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{duration: 0.5, delay: 0.3}}
+        >Connect With Me
+        </motion.h4>
+
+        <motion.h2 
+            className='text-center text-5xl font-Ovo'
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{duration: 0.5, delay: 0.5}}
+        >Get in Touch</motion.h2>
+
+        <motion.p 
+            className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.7, duration: 0.5}}
+        >
             I'd love to connect with you! Whether you have a project in mind or just want to chat, feel free to get in touch. Let's collaborate and bring your ideas to life.
-        </p>
+        </motion.p>
 
-        <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+        <motion.form onSubmit={onSubmit} 
+            className='max-w-2xl mx-auto'
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.9, duration: 0.5}}
+        >
             <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-                <input type="text" placeholder='Enter Your Name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name'/>
+                <motion.input 
+                    type="text" placeholder='Enter Your Name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name'
+                    initial={{x: -50, opacity: 0}}
+                    whileInView={{x: 0, opacity: 1}}
+                    transition={{duration: 0.6, delay: 1.1}}
+                />
 
-                <input type="email" placeholder='Enter Your Email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email'/>
+                <motion.input 
+                    type="email" placeholder='Enter Your Email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email'
+                    initial={{x: 50, opacity: 0}}
+                    whileInView={{x: 0, opacity: 1}}
+                    transition={{duration: 0.6, delay: 1.2}}
+                    />
             </div>
-            <textarea rows='6' placeholder='Enter Your Message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></textarea>
 
-            <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'>Submit Now <Image src={assets.right_arrow_white} alt='' className='w-4'/></button>
+            <motion.textarea 
+                rows='6' placeholder='Enter Your Message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1.3}}    
+            >
+            </motion.textarea>
+
+            <motion.button 
+                type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
+                whileHover={{scale: 1.05}}
+                transition={{duration: 0.3}}    
+            >Submit Now <Image src={assets.right_arrow_white} alt='' className='w-4'/
+            ></motion.button>
 
             <p className='mt-4'>{result}</p>
-        </form>
-    </div>
+        </motion.form>
+    </motion.div>
   )
 }
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,22 +1,53 @@
 import React from 'react'
 import { assets, workData } from '../../assets/assets'
 import Image from 'next/image'
+import { motion } from "motion/react"
 
 const Work = ({isDarkMode}) => {
   return (
-    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
-        <h2 className='text-center text-5xl font-Ovo'>My Latest Work</h2>
+    <motion.div id='work' 
+        className='w-full px-[12%] py-10 scroll-mt-20'
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1}}
+    >
+        <motion.h4 
+            className='text-center mb-2 text-lg font-Ovo'
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{delay: 0.3, duration: 0.3}}
+            >My Portfolio
+        </motion.h4>
 
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        <motion.h2 
+            className='text-center text-5xl font-Ovo'
+            initial={{opacity: 0, y: -20}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{duration: 0.5, delay: 0.5}}
+        >My Latest Work</motion.h2>
+
+        <motion.p 
+            className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.7, duration: 0.5}}
+        >
             Welcome to my web development portfolio! Here, you'll find a collection of my latest projects, showcasing my skills and expertise in creating engaging and user-friendly web experiences.
-        </p>
+        </motion.p>
 
-        <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
+        <motion.div 
+            className='grid grid-cols-auto my-10 gap-5 dark:text-black'
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.9, duration: 0.6}}
+        >
             {workData.map((project, index) => (
-                <div key={index} 
+                <motion.div key={index} 
                     className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
-                    style={{backgroundImage: `url(${project.bgImage})`}}>
+                    style={{backgroundImage: `url(${project.bgImage})`}}
+                    whileHover={{scale: 1.05}}
+                    transition={{duration: 0.3}}
+                    >
                     <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                         <div>
                             <h2 className='font-semibold'>{project.title}</h2>
@@ -26,13 +57,16 @@ const Work = ({isDarkMode}) => {
                             <Image src={assets.send_icon} alt='send' className='w-5'/>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             ))}
-        </div>
-        <a 
+        </motion.div>
+        <motion.a 
             className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover/50'
-            href="">Show More <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='right-arrow' className='w-4'/></a>
-    </div>
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 1.1, duration: 0.5}}
+            href="">Show More <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='right-arrow' className='w-4'/></motion.a>
+    </motion.div>
   )
 }
 
